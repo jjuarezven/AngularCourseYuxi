ef12f5474d013a7606959a803a3a211ccbea92ae
JJuarez <jose.juarez@yuxipacific.com>
2017-07-23 15:02:20 -0500

    routing

---
+++ videos/routing/js/config.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular
+    .module('app')
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        .state('home', {
+            url: '/',
+            controller: 'HomeController as ctrl',
+            template: '<p>{{ctrl.viewName}}</p>'
+        })
+        .state('about', {
+            url: '/about',
+            controller: 'AboutController as ctrl',
+            template: '<p>{{ctrl.viewName}}</p>'
+        });
+        $urlRouterProvider.otherwise('/');
+    });
+})();
